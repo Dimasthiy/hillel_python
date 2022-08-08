790f5b17a61547f4ae16a149e0a92418a6de63a1
ASKDR-Notebook3 <dimasthiy@gmail.com>
2022-08-08 09:27:12 +0300

    Homework 4

---
+++ homework/homework_4/password_check.py
@@ -0,0 +1,14 @@
+password = input("Введдіть пароль: ")
+
+valid_pass1 = ("dmytro123")
+valid_pass2 = ("tkachenko")
+valid_pass3 = ("9799464219")
+
+if password == valid_pass1:
+    print("Доступ дозволений")
+elif password == valid_pass2:
+    print("Доступ дозволений")
+elif password == valid_pass3:
+    print("Доступ дозволений")
+else:
+    print("Доступ заборонений")
